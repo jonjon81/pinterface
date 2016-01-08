e91f929aad4c4e58d7ebd19708085d75c5e07adb
Jon Atlas <jjjatlas@hotmail.com>
2016-01-08 16:09:07 -0500

    Add crud to Pin model

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,15 +1,26 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
 def index
-
+  @pins = Pin.all.order("created_at DESC")
 end
 
 def create
   @pin = Pin.new(pin_params)
+
+  if @pin.save
+    redirect_to @pin, notice: "Successfully created new Pin"
+  else
+    render 'new'
+  end
 end
 
 def update
-
+  if @pin.update(pin_params)
+    redirect_to  @pin, notice: "Pin was successfully updated!"
+  else
+    render 'edit'
+  end
 end
 
 def new
@@ -17,24 +28,23 @@ def new
 end
 
 def edit
-
 end
 
 def show
-
 end
 
 def destroy
-
+  @pin.destroy
+  redirect_to root_path
 end
 
 private
 def pin_params
-  params.require(:pin).permit(:name, :description)
+  params.require(:pin).permit(:title, :description)
 end
 
-
-
-
+def find_pin
+  @pin = Pin.find(params[:id])
+end
 
 end
